fa93ce014c9a65f7796533951ba7420cfa877060
judenieda <63973685+judenieda@users.noreply.github.com>
2020-04-24 20:30:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function
+myFunction() {
+var listItem = document.createElement("LI");
+var textItem = document.createTextNode("I like carrots!");
+listItem.appendChild(textItem);
+document.getElementById("newFact").appendChild(listItem);
+document.getElementById("button").style.display="none"
+}
