7e834caaada7a1392eea516573608a30dc5706c8
judenieda <63973685+judenieda@users.noreply.github.com>
2020-04-26 02:29:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function
 myFunction() {
 var listItem = document.createElement("LI");
-var textItem = document.createTextNode("I like carrots!");
+var textItem = document.createTextNode("The Isabella Stewrart Gardner Museum is the my favorite museum in Boston!");
 listItem.appendChild(textItem);
 document.getElementById("newFact").appendChild(listItem);
 document.getElementById("button").style.display="none"
