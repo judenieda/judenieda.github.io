971f42c2008d245798ecee3ad5ea730900d31394
judenieda <63973685+judenieda@users.noreply.github.com>
2020-04-26 04:17:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var textItem = document.createTextNode("I'm a Felxitarian!");
 listItem.appendChild(textItem);
 document.getElementById("newFact").appendChild(listItem);
 document.getElementById("button").style.display="none"}
-function 
-  changename(){
-    //Change name to nickname
-  document.getElementById(NickName).innerHTML='Juju';
-  }
+var h1item= document.getElemenyById(name);
+if (h1item.innerHTML === "Judenie") {
+    h1item.innerHTML = "Juju!";
+  } else {
+    x.innerHTML = "Judenie";  }
 
