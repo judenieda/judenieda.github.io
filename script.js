85c7274e6ad1d81a29d73f124af62a3906fce777
judenieda <63973685+judenieda@users.noreply.github.com>
2020-04-26 03:31:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ var textItem = document.createTextNode("I'm a Felxitarian!");
 listItem.appendChild(textItem);
 document.getElementById("newFact").appendChild(listItem);
 document.getElementById("button").style.display="none"
+  var h1= document.getElementById("clickName");
+  if (h1.innerHTML==="Judenie")
+    h1.innerHTML="Juju"
+  else h1.HTML="Judenie";
 }
