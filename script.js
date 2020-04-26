b2a9cb6b8a618cb9b28b82fa9da4228d74b2956a
judenieda <63973685+judenieda@users.noreply.github.com>
2020-04-26 04:38:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ var listItem = document.createElement("LI");
 var textItem = document.createTextNode("I'm a Felxitarian!");
 listItem.appendChild(textItem);
 document.getElementById("newFact").appendChild(listItem);
-document.getElementById("button").style.display="none"}
-var x= document.getElemenyById("myDiv);
-        if(x.innerHTML=="Junebug"){
-        x.innerHTML="Juju";}
-        else{x.innerHTML="Junebug";
-    }
+document.getElementById("button").style.display="none";
                                
  }
 
